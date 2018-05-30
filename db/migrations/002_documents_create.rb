2b241693600c56892abc61b2087e8fc97edda6cb
Muzi P. Gondwe <phiwokuhlegondwe@gmail.com>
2018-05-30 22:51:59 +0800

    Solving migration issue

--- db/migrations/002_documents_create.rb
+++ db/migrations/002_documents_create.rb
@@ -5,7 +5,7 @@ require 'sequel'
 Sequel.migration do
   change do
     create_table(:documents) do
-      uuid :id, primary_key: true
+      primary_key :id
       foreign_key :owner_id, :accounts
 
       String :filename_secure, unique: true, null: false
