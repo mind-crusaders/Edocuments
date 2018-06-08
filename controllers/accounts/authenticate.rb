0832a307f9db0f13743b9fa1d6bdc96ab53e9780
orison09 <orison09@gmail.com>
2018-06-08 11:19:41 +0800

    Finding Errors

--- controllers/accounts/authenticate.rb
+++ controllers/accounts/authenticate.rb
@@ -1,8 +1,9 @@
+
 # frozen_string_literal: true
 
 require 'roda'
 
-module Edocument
+module Credence
   # Web controller for Credence API
   class Api < Roda
     route('authenticate', 'accounts') do |routing|
@@ -11,7 +12,10 @@ module Edocument
         credentials = JsonRequestBody.parse_symbolize(request.body.read)
         auth_account = AuthenticateAccount.call(credentials)
         auth_account.to_json
-      end 
+      rescue StandardError => error
+        puts "ERROR: #{error.class}: #{error.message}"
+        routing.halt '403', { message: 'Invalid credentials' }.to_json
+      end
     end
   end
 end
