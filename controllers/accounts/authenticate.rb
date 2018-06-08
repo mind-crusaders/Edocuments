c46bd5500fa861359d1388ab1819d7066ed3df74
orison09 <orison09@gmail.com>
2018-06-08 10:39:43 +0800

    Finding errors

--- controllers/accounts/authenticate.rb
+++ controllers/accounts/authenticate.rb
@@ -10,12 +10,7 @@ module Edocument
       routing.post do
         credentials = JsonRequestBody.parse_symbolize(request.body.read)
         auth_account = AuthenticateAccount.call(credentials)
-        auth_account.to_json
-
-        rescue StandardError => error
-          puts "ERROR: #{error.class}: #{error.message}" 
-        routing.halt '403', { message: 'Invalid credentials' }.to_json
-      end
+        auth_account.to_json 
     end
   end
 end
