4fb63b12765052cb144ea1948fcab67911755659
Muzi P. Gondwe <phiwokuhlegondwe@gmail.com>
2018-06-13 23:46:55 +0800

    Worked on autheticate.rb

--- controllers/accounts/authenticate.rb
+++ controllers/accounts/authenticate.rb
@@ -12,9 +12,9 @@ module Edocument
         credentials = JsonRequestBody.parse_symbolize(request.body.read)
         auth_account = AuthenticateAccount.call(credentials)
         auth_account.to_json
-      rescue UnauthorizedError => error
-        puts [error.class, error.message].join ': '
-        routing.halt '403', { message: 'Invalid credentials' }.to_json
+      #rescue UnauthorizedError => error
+       # puts [error.class, error.message].join ': '
+        #routing.halt '403', { message: 'Invalid credentials' }.to_json
       end
     end
   end
